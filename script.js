ab2ce1f81eef041829b38bbd77db6a5ba2d7ec03
Sherm1337 <162771748+Sherm1337@users.noreply.github.com>
2024-03-27 01:34:38 -0500

    Add hover effect and css effects
    - Added mouse hover effect when curser is over divs
    inside the sketch box
    - Changed color theme of page.

--- script.js
+++ script.js
@@ -1,30 +1,31 @@
-const changeGrid = document.querySelector("#changeGrid");
+const createGrid = document.querySelector("#createGrid");
 const resetGrid = document.querySelector("#resetGrid");
 const gridContainer = document.querySelector(".gridContainer");
-const squares = document.querySelector(".square");
+const square = document.querySelector(".square");
+const divs = document.querySelector("div");
 
-changeGrid.addEventListener("click", function gridSize() {
+createGrid.addEventListener("click", function gridSize() {
 
     let gridNum = prompt("Enter an amount:");
-    
-    let gridSize = gridNum * gridNum;
 
-    for (var i = 0; i < gridSize; i++) {
-        const newSquare = document.createElement("div");
-        newSquare.className = "square";
-        gridContainer.appendChild(newSquare);
+    if (gridNum > 100 || gridNum !== Number) {
+        alert("Please enter a number under 100.");
+    } else {
+        let gridSize = gridNum * gridNum;
 
-        newSquare.style.width = (960 / gridNum) + "px";
-        newSquare.style.height = (960 / gridNum) + "px";
+        for (var i = 0; i < gridSize; i++) {
+            const newSquare = document.createElement("div");
+            newSquare.className = "square";
+            gridContainer.appendChild(newSquare);
+
+            newSquare.style.width = (960 / gridNum) + "px";
+            newSquare.style.height = (960 / gridNum) + "px";
+        }
     }
-
-   
-    
 });
 
 resetGrid.addEventListener("click", function reset () {
     while (gridContainer.firstChild) {
         gridContainer.removeChild(gridContainer.firstChild);
     }
-
-})
+});
