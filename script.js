d76f0c046cb0632b172717a5d82f5c54d2723029
Sherm1337 <162771748+Sherm1337@users.noreply.github.com>
2024-03-27 19:28:52 -0500

    Edit JS hover effect
    Created new JS color effect for when mouse moves
    over the grid squares.

--- script.js
+++ script.js
@@ -4,9 +4,17 @@ const gridContainer = document.querySelector(".gridContainer");
 const square = document.querySelector(".square");
 const divs = document.querySelector("div");
 
+function draw(e) {
+    e.target.style.background = "chartreuse";
+    opacity = e.target.style.opacity;
+    e.target.style.opacity = (parseFloat(opacity) + 0.1);
+}
+
+setTimeout(draw, 20000);
+
 createGrid.addEventListener("click", function gridSize() {
 
-    let gridNum = prompt("Enter an amount:");
+    let gridNum = prompt("Enter an amount. 100 being the smallest brush size:");
 
     if (gridNum > 100) {
         alert("Please enter a number under 100.");
@@ -15,17 +23,21 @@ createGrid.addEventListener("click", function gridSize() {
 
         for (var i = 0; i < gridSize; i++) {
             const newSquare = document.createElement("div");
-            newSquare.className = "square";
+            newSquare.setAttribute("class", "square");
             gridContainer.appendChild(newSquare);
 
             newSquare.style.width = (960 / gridNum) + "px";
             newSquare.style.height = (960 / gridNum) + "px";
-        }
-    }
-});
+            newSquare.style.opacity = "0";
+            
+            resetGrid.addEventListener("click", () => {
+                newSquare.style.background = "transparent";
+            })
 
-resetGrid.addEventListener("click", function reset () {
-    while (gridContainer.firstChild) {
-        gridContainer.removeChild(gridContainer.firstChild);
+            newSquare.addEventListener("mouseover", draw);
+
+
+        }
+        document.querySelectorAll(square);
     }
 });
